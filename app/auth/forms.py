5fc829552135ac679bd0f7f4109cda35dad1adac
Abhishek <abhishek.pandey.3237@gmail.com>
2021-09-25 23:04:12 +0530

    Added Registration Option

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,28 @@
+from wtforms.fields.core import IntegerField
+from app.models import Hospital
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,\
-                    SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-            validators=[DataRequired(),Length(1,64),Email()])
-    password = PasswordField('Password',
-            validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+        email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        remember_me = BooleanField('Keep me logged in')
+        submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+        name = StringField('Name', validators=[DataRequired(), Length(1,64)])
+        email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+        pincode = IntegerField('PinCode', validators=[DataRequired()])
+        password = PasswordField('Enter Password', 
+                                        validators=[DataRequired(), 
+                                                EqualTo('repassword', message="Passwords must match")])
+        repassword = PasswordField('Re-enter Password',
+                                        validators=[DataRequired()])
+        submit = SubmitField('Register')
+
+        def verify_email(self, email):
+                if Hospital.query.filter_by(email=email.data).first():
+                        raise ValidationError("User already registered")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,12 @@
-from flask import render_template, redirect, url_for, request, flash, session
+from flask import render_template, url_for, request, flash, session
 from flask_login import login_user, logout_user, login_required
 from flask_login.utils import login_required
 from werkzeug.utils import redirect
 
 from app.models import Hospital
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
@@ -23,10 +24,25 @@ def login():
             flash("Invalid Username / Password")
     return render_template('auth/login.html',form=form),200
 
-@auth.route('logout')
+@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash("You have been logged out")
     session['name'] = "Normal User"
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hospital = Hospital(name=form.name.data,
+        email=form.email.data,
+        pincode=form.pincode.data,
+        password=form.password.data)
+        db.session.add(hospital)
+        db.session.commit()
+
+        flash("You can now login !")
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
